5d1796d1b34e2d102141961e7c1031b3df88f7b1
Bultez Jordan <jordan.bultez@gmail.com>
2015-12-03 18:30:11 +0100

    localstorage end git add script.jsgit add script.jsgit add script.jsgit add script.jsgit add script.js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+window.onload = function(e){
+  if(typeof localStorage!='undefined') {
+    for (numeroTeam=1; numeroTeam<=6 ; numeroTeam++) {
+      var team = localStorage.getItem("nomTeam" + numeroTeam);
+      if (team != null) {
+        document.getElementById("nomTeam" + numeroTeam).innerHTML = team;
+      }
+
+      for (i=1; i<=5 ; i++) {
+        var player = localStorage.getItem("team" + numeroTeam + "J" + i);
+        if (player != null) {
+          document.getElementById("team" + numeroTeam +"J"+ i).innerHTML = player;
+        }
+      }
+    }
+  }
+}
+
 function ask(numeroTeam){
   for (i=1; i<=5; i++){
-  var player = document.getElementById("inputTeam" + numeroTeam + "J" + i).value;
-  document.getElementById("team" + numeroTeam + "J" + i).innerHTML = player;
-}
+    var player = document.getElementById("inputTeam" + numeroTeam + "J" + i).value;
+    document.getElementById("team" + numeroTeam + "J" + i).innerHTML = player;
+
+    localStorage.setItem("team" + numeroTeam + "J" + i, player);
+    player = localStorage.getItem("team" + numeroTeam + "J" + i);
+  }
   var team = document.getElementById("inputNomTeam"+ numeroTeam).value;
   document.getElementById("nomTeam" + numeroTeam).innerHTML = team;
+
+  localStorage.setItem("nomTeam"+ numeroTeam, team);
 }
