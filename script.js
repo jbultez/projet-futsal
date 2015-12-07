f57aaffd9ed0b80081426235369394d9fd1c98e9
MACIEJEWSKI Jessica <maciejewski.j@pop.eu.com>
2015-12-07 16:20:06 +0100

    correction JS+ nom id

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.onload = function(e){
   if(typeof localStorage!='undefined') {
     for (numeroTeam=1; numeroTeam<=6 ; numeroTeam++) {
-      var team = localStorage.getItem("nomTeam" + numeroTeam);
+      var team = localStorage.getItem("nameTeam" + numeroTeam);
       if (team != null) {
-        document.getElementById("nomTeam" + numeroTeam).innerHTML = team;
+        document.getElementsByName("nameTeam" + numeroTeam).innerHTML = team;
       }
 
       for (i=1; i<=5 ; i++) {
-        var player = localStorage.getItem("team" + numeroTeam + "J" + i);
+        var player = localStorage.getItem("nameTeam" + numeroTeam + "Player" + i);
         if (player != null) {
-          document.getElementById("team" + numeroTeam +"J"+ i).innerHTML = player;
+          document.getElementsByName("nameTeam" + numeroTeam +"Player"+ i).innerHTML = player;
         }
       }
     }
@@ -18,14 +18,18 @@ window.onload = function(e){
 
 function ask(numeroTeam){
   for (i=1; i<=5; i++){
-    var player = document.getElementById("inputTeam" + numeroTeam + "J" + i).value;
-    document.getElementById("team" + numeroTeam + "J" + i).innerHTML = player;
+    var player = document.getElementById("inputNameTeam" + numeroTeam + "Player" + i).value;
+    document.getElementById("nameTeam" + numeroTeam + "Player" + i).innerHTML = player;
 
-    localStorage.setItem("team" + numeroTeam + "J" + i, player);
-    player = localStorage.getItem("team" + numeroTeam + "J" + i);
+    localStorage.setItem("nameTeam" + numeroTeam + "Player" + i, player);
+    player = localStorage.getItem("nameTeam" + numeroTeam + "Player" + i);
   }
-  var team = document.getElementById("inputNomTeam"+ numeroTeam).value;
-  document.getElementById("nomTeam" + numeroTeam).innerHTML = team;
+  var team = document.getElementById("inputNameTeam"+ numeroTeam);
+  elems = document.getElementsByName("nameTeam" + numeroTeam);
+  for (i = 0; i < elems.length; i++) {
+    elems[i].innerHTML = team.value;
 
-  localStorage.setItem("nomTeam"+ numeroTeam, team);
+
+  localStorage.setItem("nameTeam"+ numeroTeam, team);
+}
 }
